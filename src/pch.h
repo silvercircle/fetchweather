184ca73833111525f713b5ac3f058d0e062ac937
Alex <silvercircle@gmail.com>
2021-03-02 06:47:20 +0100

    initial commit

---
+++ src/pch.h
@@ -0,0 +1,55 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Alex Vie (silvercircle@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * precompiled headers are produced from this file.
+ */
+
+
+#ifndef OBJCTEST_PCH_H
+#define OBJCTEST_PCH_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <memory>
+#include <filesystem>
+#include <chrono>
+#include <ctime>
+#include <glib-2.0/glib-unix.h>
+#include <sqlite3.h>
+#include <curl/curl.h>
+#include "CLI/App.hpp"
+#include "CLI/Formatter.hpp"
+#include "CLI/Config.hpp"
+#include "nlohmann/json.hpp"
+#include "loguru/loguru.hpp"
+#include "conf.h"
+#include "options.h"
+#include "DataHandler.h"
+#include "DataHandler_ImplClimaCell.h"
+#include "DataHandler_ImplOWM.h"
+
+namespace fs = std::filesystem;
+using namespace nlohmann;
+
+#endif //OBJCTEST_PCH_H
