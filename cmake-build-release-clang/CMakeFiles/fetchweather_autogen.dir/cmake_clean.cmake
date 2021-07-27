f5700111329875d36b96bcef6c8e6b0e043c8ee2
Alex <silvercircle@gmail.com>
2021-07-27 04:56:55 +0200

    fixes

--- cmake-build-release-clang/CMakeFiles/fetchweather_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/fetchweather_autogen"
-  "fetchweather_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/fetchweather_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
